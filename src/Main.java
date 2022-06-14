9910f397e742aa373c68c9fcfcddde0b07e8f6b3
Jiseeeh <johncarlo.camara1@gmail.com>
2022-06-15 01:26:10 +0800

    Remove TController static method

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
-import Controller.TeacherController;
 import Prompt.StudentSystem;
 
 public class Main {
     public static void main(String[] args) {
         StudentSystem.loadAccounts();
-        TeacherController.checkForTasksAndFeeds();
         StudentSystem.prompt();
     }
 }
