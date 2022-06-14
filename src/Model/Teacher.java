9d64bf69eed03530a5fc32be4f77c19a9acf14aa
Jiseeeh <johncarlo.camara1@gmail.com>
2022-06-15 01:24:56 +0800

    Move checking of feeds/tasks to teacher constructor

--- src/Model/Teacher.java
+++ src/Model/Teacher.java
@@ -1,11 +1,16 @@
 package Model;
 
+import Helper.FileHelper;
+
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Teacher extends User {
-    private static final List<Feedback> givenFeeds = new ArrayList<>();
-    private static final List<Task> givenTasks = new ArrayList<>();
+    private static final File feedsCSV = new File("src/Database/CSV/feeds.csv");
+    private static final File tasksCSV = new File("src/Database/CSV/tasks.csv");
+    private final List<Feedback> givenFeeds = new ArrayList<>();
+    private final List<Task> givenTasks = new ArrayList<>();
 
     public Teacher(UserBuilder builder) {
         this.setFirstName(builder.getFirstName());
@@ -14,6 +19,8 @@ public class Teacher extends User {
         this.setPassword(builder.getPassword());
         this.setAge(builder.getAge());
         this.setType(builder.getType());
+        FileHelper.checkForFeeds(feedsCSV, givenFeeds);
+        FileHelper.checkForTasks(tasksCSV, givenTasks);
     }
 
     public List<Feedback> getGivenFeeds() {
@@ -23,4 +30,13 @@ public class Teacher extends User {
     public List<Task> getGivenTasks() {
         return givenTasks;
     }
+
+    public File getFeedsCSV() {
+        return feedsCSV;
+    }
+
+    public File getTasksCSV() {
+        return tasksCSV;
+    }
+
 }
